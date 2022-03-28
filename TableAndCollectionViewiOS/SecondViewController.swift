0c397867eb02caaf11f68c28fd8b6fdb82c17038
leonarrdo404 <leo@ese.plus>
2022-03-27 22:49:41 -0600

    collectionView demo pokemon

--- TableAndCollectionViewiOS/SecondViewController.swift
+++ TableAndCollectionViewiOS/SecondViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class SecondViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
+    private var pokemons: [Pokemon] = [
+        Pokemon(name: "Pikachu", image: "", isCf: true),
+        Pokemon(name: "Bulbasor", image: "", isCf: false),
+        Pokemon(name: "Voltor", image: "", isCf: true),
+        Pokemon(name: "Mew", image: "", isCf: false)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,20 +31,20 @@ class SecondViewController: UIViewController {
 // MARK: - ICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout - iOS 12<
 extension SecondViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return pokemons.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if indexPath.row % 2 == 0 {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mySecondCell", for: indexPath) as? MySecondCell
-            cell?.titleLabel.text = "a \(indexPath)"
-            
-            return cell!
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mySecondCell", for: indexPath) as? MySecondCell
+        cell?.titleLabel.text = "a \(indexPath)"
+        
+        if pokemons[indexPath.row].isCf == true {
+            cell?.colorView.backgroundColor = UIColor.green
         } else {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myThirdCell", for: indexPath) as? MyThirdCell
-            
-            return cell!
+            cell?.colorView.backgroundColor = UIColor.gray
         }
+        
+        return cell!
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
---
+++ TableAndCollectionViewiOS/View/Model/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  TableAndCollectionViewiOS
+//
+//  Created by Leo Lopez on 27/3/22.
+//
+
+import Foundation
+
+struct Pokemon {
+    let name: String
+    let image: String
+    let isCf: Bool
+}
