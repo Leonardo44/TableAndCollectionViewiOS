cae8ead30819d136edff31f0164a2bd1d75b202f
leonarrdo404 <leo@ese.plus>
2022-03-27 21:25:26 -0600

    Initial Commit

---
+++ TableAndCollectionViewiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableAndCollectionViewiOS
+//
+//  Created by Leo Lopez on 27/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
